f5458b8727088633082c86d5312ac2b6929d9b2a
tmxhsk99 <tmxhsk99@naver.com>
2022-07-16 11:06:33 +0900

    refactor : 함수 추출하기 적용 6-1

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,27 +1,35 @@
-export function printOwing(invoice) {
-  let outstanding = 0;
-
+function printBanner() {
+  // 배너를 출력
   console.log('***********************');
   console.log('**** Customer Owes ****');
   console.log('***********************');
+}
+// 총가격을 계산
+function calculateOutstanding(invoice) {
+  return invoice.orders.reduce((sum,order) => (sum += order.amount),0);
+}
 
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
-
-  // record due date
+// 지급 날짜를 계산
+function recordDueDate(invoice) {
   const today = new Date();
   invoice.dueDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 30
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + 30
   );
-
-  //print details
+}
+//세부사항을 출력
+function printDetail(invoice, outstanding) {
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+export function printOwing(invoice) {
+  printBanner();
+  let outstanding = calculateOutstanding(invoice);
+  recordDueDate(invoice);
+  printDetail(invoice, outstanding);
 }
 
 const invoice = {
