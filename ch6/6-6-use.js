4bab6c8b690960cfd8d0c6023d7c05a5287e5033
tmxhsk99 <tmxhsk99@naver.com>
2022-07-16 16:34:20 +0900

    refactor : 6.6 변수 캡슐화하기

--- ch6/6-6-use.js
+++ ch6/6-6-use.js
@@ -1,6 +1,5 @@
 import { getDefaultOwner } from './6-6.js';
 
 const owner = getDefaultOwner();
-owner.firstName = '엘리';
-console.log(owner);
+console.log(owner.firstName);
 console.log(getDefaultOwner());
--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,5 +1,22 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+class Person {
+  #lastName;
+  #firstName;
+  constructor(data) {
+    this.#lastName = data.lastName;
+    this.#firstName = data.firstName;
+  }
+  get lastName(){
+    return this.#lastName;
+  }
+  get firstName(){
+    return this.#firstName;
+  }
+}
+
+let defaultOwner = new Person({ firstName: '마틴', lastName: '파울러' });
 
 export function getDefaultOwner() {
+  //스프레이드 연산자 사용시 얕은 복사
+  //return {...defaultOwner};
   return defaultOwner;
 }
