7a7ecdc0f69297b946c6b254431a3730150c71d9
tmxhsk99 <tmxhsk99@naver.com>
2022-07-16 15:58:33 +0900

    refactor : 6.5 함수의 선언 바꾸기

--- ch6/6-5-1.js
+++ ch6/6-5-1.js
@@ -1,3 +1,4 @@
-export function circum(radius) {
+export function circumference(radius) {
+  ///원주율 구하는 함수이름이 원으로 되었어서 이름을 똑바로 변경해줌
   return 2 * Math.PI * radius;
 }
--- ch6/6-5-3.js
+++ ch6/6-5-3.js
@@ -1,3 +1,5 @@
-export function inNewEngland(aCustomer) {
-  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
+//함수내부에서 필요로하는 변수만 받아오게 하므로 함수의 재사용성을 높임
+//근데 객체단위로만 많이 쓰는 경우는 안해도 된다. 책에나옴
+export function inNewEngland(state) {
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(state);
 }
