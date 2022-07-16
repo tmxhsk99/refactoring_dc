2b0a9879c1bcc5b4a4afc0eeeba9eb4d53f1b83b
tmxhsk99 <tmxhsk99@naver.com>
2022-07-16 12:24:15 +0900

    refactor : 변수 추출하기 6-3-1

--- ch6/6-3-1.js
+++ ch6/6-3-1.js
@@ -1,8 +1,7 @@
 export function price(order) {
-  // 가격(price) = 기본가격 - 수량할인 + 배송비
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+    // 가격(price) = 기본가격 - 수량할인 + 배송비
+    let basePrice = order.quantity * order.itemPrice;
+    let discount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+    let shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+    return basePrice - discount + shipping;
 }
