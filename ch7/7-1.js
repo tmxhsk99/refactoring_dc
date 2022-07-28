f40fc04f5b7885a32294b8350507f0b5388367d1
tmxhsk99 <tmxhsk99@naver.com>
2022-07-28 19:08:38 +0900

    refactor : 7.1 레코드 캡슐화하기

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,35 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+    #name;
+    #country;
+    #data;
+
+    constructor(data) {
+        this.#name = data.name;
+        this.#country = data.country;
+        this.#data = data;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    set name(value) {
+        this.#name = value;
+    }
+
+    get country() {
+        return this.#country;
+    }
+
+    get rawData() {
+        return {name : this.name , country: this.country};
+    }
+
+}
+const data =  { name: 'Acme Gooseberries', country: 'GB' };
+const organization = new Organization(data);
 
 organization.name = 'Dream Coding';
 console.log(organization.name);
 console.log(organization.country);
+console.log(organization.rawData);
