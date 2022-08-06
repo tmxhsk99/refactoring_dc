fe892b328705ab57d890373872e4a54c2a5bfab4
tmxhsk99 <tmxhsk99@naver.com>
2022-08-06 10:13:39 +0900

    refactor : 7.5 클래스 추출하기

--- ch7/7-5.js
+++ ch7/7-5.js
@@ -1,25 +1,15 @@
-class Person {
-  #name;
-  #officeAreaCode;
-  #officeNumber;
-  constructor(name, areaCode, number) {
-    this.#name = name;
-    this.#officeAreaCode = areaCode;
-    this.#officeNumber = number;
+class TelephoneNumber {
+   #officeAreaCode;
+   #officeNumber;
+
+  constructor(officeAreaCode, officeNumber) {
+    this.#officeAreaCode = officeAreaCode;
+    this.#officeNumber = officeNumber;
   }
 
-  get name() {
-    return this.#name;
-  }
-
-  set name(arg) {
-    this.#name = arg;
-  }
-
-  get telephoneNumber() {
-    return `(${this.officeAreaCode}) ${this.officeNumber}`;
-  }
-
+  get toString() {
+      return `(${this.officeAreaCode}) ${this.officeNumber}`;
+    }
   get officeAreaCode() {
     return this.#officeAreaCode;
   }
@@ -36,6 +26,46 @@ class Person {
     this.#officeNumber = arg;
   }
 }
+
+class Person {
+  #name;
+  #telephoneNumber;
+
+  constructor(name, areaCode, number) {
+    this.#name = name;
+    this.#telephoneNumber = new TelephoneNumber(areaCode,number);
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set name(arg) {
+    this.#name = arg;
+  }
+
+  get telephoneNumber() {
+    return this.#telephoneNumber.toString;
+  }
+
+  get officeAreaCode() {
+    return this.#telephoneNumber.officeAreaCode;
+  }
+
+  set officeAreaCode(arg) {
+    this.#telephoneNumber.officeAreaCode = arg;
+  }
+
+  get officeNumber() {
+    return this.#telephoneNumber.officeNumber;
+  }
+
+  set officeNumber(arg) {
+    this.#telephoneNumber.officeNumber = arg;
+  }
+}
+
+
 
 const person = new Person('엘리', '010', '12345678');
 console.log(person.name);
