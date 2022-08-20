a985311d19031777ea1ff6427d40e7b7caf3126a
tmxhsk99 <tmxhsk99@naver.com>
2022-08-20 14:33:02 +0900

    refactor : 8.6 문장 슬라이드하기

--- ch8/8-6.js
+++ ch8/8-6.js
@@ -1,18 +1,14 @@
 // 예제 1
 const pricingPlan = retrievePricingPlan();
+const chargePerUnit = pricingPlan.unit;
 const order = retreiveOrder();
 let charge;
-const chargePerUnit = pricingPlan.unit;
 
 // 예제 2
 function someFunc() {
-  let result;
-  if (availableResources.length === 0) {
-    result = createResource();
+    let result = availableResources.length === 0
+        ? createResource()
+        : availableResources.pop();
     allocatedResources.push(result);
-  } else {
-    result = availableResources.pop();
-    allocatedResources.push(result);
-  }
-  return result;
+    return result;
 }
