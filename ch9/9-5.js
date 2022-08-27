af0620db67ba9e7384c201264d9c1d53f6999617
tmxhsk99 <tmxhsk99@naver.com>
2022-08-27 20:36:32 +0900

    refactor : 9.5 값을 참조로 바꾸기

--- ch9/9-5.js
+++ ch9/9-5.js
@@ -1,7 +1,11 @@
+const customerRepository = new CustomerRepository();
+//클라이언트 소스에서 리포지터리에 접근하도록 의존성을 도메인에서 분리함
+const order = new Order(data.number, customerRepository.registerCustomer(data.custoerId));
+
 class Order {
-  constructor(data) {
-    this._number = data.number;
-    this._customer = new Customer(data.customerId);
+  constructor(number, customer) {
+    this._number = number;
+    this._customer = customer;
   }
 
   get customer() {
@@ -10,11 +14,32 @@ class Order {
 }
 
 class Customer {
-  constructor(id) {
+  constructor(id, name) {
     this._id = id;
+    this._name = name;
   }
 
   get id() {
     return this._id;
+  }
+}
+
+//리포지터리 패턴 일종의 DB 같은 느낌 Map 으로 구현되어있음
+class CustomerRepository {
+  #customers;
+
+  constructor() {
+    this.#customers = new Map();
+  }
+
+  registerCustomer(id) {
+    if (!this.#customers.has(id)) {
+      this.#customers.set(id, new Customer(id));
+    }
+    return findCustomer(id);
+  }
+
+  findCustomer(id) {
+    return this.#customers.get(id);
   }
 }
