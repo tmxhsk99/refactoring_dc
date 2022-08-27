582f6e85b0956a3359e5366748c70d5a775cbc52
tmxhsk99 <tmxhsk99@naver.com>
2022-08-27 20:39:20 +0900

    refactor : 9.6 매직리터럴 바꾸기

--- ch9/9-6.js
+++ ch9/9-6.js
@@ -1,3 +1,7 @@
+const STANDARD_GRAVITY = 9.81;
 function potentialEnergy(mass, height) {
-  return mass * 9.81 * height;
+  return mass * STANDARD_GRAVITY * height;
 }
+//의미없는 숫자는 상수로 정의해서 어떤의미인지 명확하게 한다.
+const MAX_ITEMS = 5;
+item * MAX_ITEMS;
