e94945731344ca456f51a9b0fef6cabdc475b209
tmxhsk99 <tmxhsk99@naver.com>
2022-08-27 19:47:34 +0900

    refactor : 9.2 필드 이름바꾸기

--- ch9/9-2.js
+++ ch9/9-2.js
@@ -1,12 +1,13 @@
+//클래스 안의 필드명이 모호하거나 문맥적으로 애매하면 변경할 것
 class Organization {
   constructor(data) {
     this._name = data.name;
     this._country = data.country;
   }
-  get name() {
+  get title() {
     return this._name;
   }
-  set name(value) {
+  set title(value) {
     this._name = value;
   }
   get country() {
