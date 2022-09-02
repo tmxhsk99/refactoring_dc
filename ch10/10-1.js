840f01fd58bfdd172eae8169b5154ad226fcb3c4
tmxhsk99 <tmxhsk99@naver.com>
2022-09-02 23:56:01 +0900

    refactor : 10.1 조건문 분해하기

--- ch10/10-1.js
+++ ch10/10-1.js
@@ -1,7 +1,20 @@
 function calculateCharge(date, quantity, plan) {
   let charge = 0;
-  if (!date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd))
-    charge = quantity * plan.summerRate;
-  else charge = quantity * plan.regularRate + plan.regularServiceCharge;
+
+  function summer() {
+    return !date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd);
+  }
+
+  function summerCharge() {
+    return quantity * plan.summerRate;
+  }
+
+  function regularCharge() {
+    return quantity * plan.regularRate + plan.regularServiceCharge;
+  }
+
+  if (summer())
+    charge = summerCharge();
+  else charge = regularCharge();
   return charge;
 }
