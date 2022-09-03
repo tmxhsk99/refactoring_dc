954bd728f162036151b6cebfba6d4fca16830e8c
tmxhsk99 <tmxhsk99@naver.com>
2022-09-04 07:16:14 +0900

    refactor : 10.3 중첩 조건문을 보호 구문으로 바꾸기 (Replace Nested Conditional with Guard Clauses)

--- ch10/10-3-2.js
+++ ch10/10-3-2.js
@@ -1,11 +1,7 @@
 export function adjustedCapital(instrument) {
-  let result = 0;
-  if (instrument.capital > 0) {
-    if (instrument.interestRate > 0 && instrument.duration > 0) {
-      result =
-        (instrument.income / instrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
-  }
-  return result;
+    return isElegiableForAdjuestedCaptital(instrument) ? (instrument.income / instrument.duration) * anInstrument.adjustmentFactor : 0;
+}
+
+function isElegiableForAdjuestedCaptital(instrument) {
+    return instrument.capital > 0 && instrument.interestRate > 0 && instrument.duration > 0;
 }
