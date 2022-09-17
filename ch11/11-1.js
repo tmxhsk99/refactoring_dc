01416181e35236ba8c16196a05ab793e55270fd1
tmxhsk99 <tmxhsk99@naver.com>
2022-09-17 20:12:26 +0900

    refactor : 11.1 질의 함수와 변경 함수 분리하기 (Separate Query from Modifier)

--- ch11/11-1.js
+++ ch11/11-1.js
@@ -1,28 +1,29 @@
 // 예제 1
-function totalOutstandingAndSendBill() {
-  const result = customer.invoices.reduce(
-    (total, each) => each.amount + total,
-    0
-  );
-  sendBill();
-  return result;
+function getTotalOutstanding() {
+  return customer.invoices.reduce((total, each) => each.amount + total, 0);
+}
+
+function sendBill() {
+  // bill을 보냄
 }
 
 // 예제 2
 export function alertForMiscreant(people, alarm) {
+  const miscreant = findMiscreant(people);
+  setOffAlarms(alarm, miscreant);
+}
+
+function findMiscreant(people) {
   for (const p of people) {
     if (p === 'Don') {
-      setOffAlarms(alarm, p);
       return 'Don';
     }
     if (p === 'John') {
-      setOffAlarms(alarm, p);
       return 'John';
     }
   }
   return '';
 }
-
 function setOffAlarms(alarm, p) {
   alarm.setOff('Found Miscreant ' + p);
 }
